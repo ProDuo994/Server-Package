d10d22a3f1d2f4ee0e267d7220cdaea8b760609e
ProDuo994 <116141375+ProDuo994@users.noreply.github.com>
2024-04-21 14:27:22 +1000

    Update client.py

--- backend/client.py
+++ backend/client.py
@@ -4,3 +4,8 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!disconnect"
+SERVER = "192.168.68.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
