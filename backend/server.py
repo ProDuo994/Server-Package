264f4f59b34ed0dac3ba7eb500ccb98215e34fff
ProDuo994 <116141375+ProDuo994@users.noreply.github.com>
2024-04-21 13:57:02 +1000

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -30,6 +30,7 @@ def handle_client(conn, addr):
 
 def start():
   server.listen()
+  print(f"[LISTENING] Server is listening on {SERVER}")
   while True:
     conn, addr = server.accept()
     thread = threading.Thread(target=handle_client(), args=(conn, addr))
