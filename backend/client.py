31c2f572e8f8cdecc35c15716ecc9d270f620169
ProDuo994 <116141375+ProDuo994@users.noreply.github.com>
2024-04-22 12:41:59 +1000

    Update client.py

--- backend/client.py
+++ backend/client.py
@@ -7,5 +7,24 @@ DISCONNECT_MESSAGE = "!disconnect"
 SERVER = "192.168.68.1"
 ADDR = (SERVER, PORT)
 
+class client:
+  log = {}
+  def _init_(self, ip, status):
+    self.ip = ip
+    self.status = status
+
+  def remove():
+    pass
+
+  def send(addr, format, port, msg):
+    log.append("Sent " + msg + " to " + ip)
+
+  def getHistory()
+    pass
+
+def onConnect():
+  pass
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+currentClient = client("192.168.68.1", "online")
